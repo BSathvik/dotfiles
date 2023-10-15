81b9d846cf9e4142e84c4bdf20e2a60be7a20bab
Sathvik Birudavolu <8377116+BSathvik@users.noreply.github.com>
2023-10-15 12:06:52 +0530

    rm lsp.buf.format

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -367,9 +367,6 @@ require("lazy").setup({
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<space>f", function()
-          vim.lsp.buf.format({ async = true })
-        end, opts)
       end
 
       -- nvim-cmp supports additional completion capabilities
