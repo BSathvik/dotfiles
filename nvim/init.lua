4d8b635940d526bd4e76073f0d927b206e706e32
Sathvik Birudavolu <sathvikb30@gmail.com>
2024-10-02 00:49:13 -0400

    add iron

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,6 +112,67 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "Vigemus/iron.nvim",
+    event = "VeryLazy",
+
+    config = function()
+      local iron = require("iron.core")
+
+      iron.setup({
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { "zsh" },
+            },
+            python = {
+              command = { "python" }, -- or { "ipython", "--no-autoindent" }
+              format = require("iron.fts.common").bracketed_paste_python,
+            },
+          },
+          -- How the repl window will be displayed
+          -- See below for more information
+          repl_open_cmd = require("iron.view").bottom(20),
+        },
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          send_motion = "<leader>ms",
+          visual_send = "<leader>mc",
+          send_file = "<leader>mf",
+          send_line = "<leader>ml",
+          -- send_paragraph = "<leader>xp",
+          -- send_until_cursor = "<leader>xu",
+          -- send_mark = "<leader>xm",
+          -- mark_motion = "<leader>mc",
+          -- mark_visual = "<leader>mc",
+          -- remove_mark = "<leader>md",
+          -- cr = "<leader>s<cr>",
+          -- interrupt = "<leader>s<space>",
+          -- exit = "<leader>sq",
+          -- clear = "<leader>cl",
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      })
+
+      -- iron also has a list of commands, see :h iron-commands for all available commands
+      vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
+      vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+      vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+      vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+    end,
+  },
+
   -- UI to select things (files, grep results, open buffers...)
   {
     "nvim-telescope/telescope.nvim",
